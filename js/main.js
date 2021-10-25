c0511f2548fabb2b595e1cba937ae769c43525ac
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-25 16:48:49 +0200

    aggiunto analisi programma js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@
 // per i multipli di 3,
 // per i multipli di 5
 // e per i valori che sono sia multipli di 3 che di 5.
+
+// dichiarazioni variabili
+
+// con un ciclo for genero degli elementi in pagina con classe square e come contenuto il numero iterazione
+    // - verifico se il numero del quadrato è divisibile per 3
+        // * aggiungo la classe fizz e modifico il contenuto con la parole fizz
+    // - verifico se il numero del quadrato è divisibile per 5        
+        // * aggiungo la classe buzz e modifico il contenuto con la parole buzz
+    // - se il numero è divisibile per 3 e per 5 aggiungo entrambe le classi ed entrambe le parole
