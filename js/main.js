c651cefdccc780f24f735d56b0306bfbf919a1d3
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-25 17:23:44 +0200

    esercio terminato

--- js/main.js
+++ js/main.js
@@ -5,10 +5,34 @@
 // e per i valori che sono sia multipli di 3 che di 5.
 
 // dichiarazioni variabili
+const squareContainer = document.getElementById("container");
 
 // con un ciclo for genero degli elementi in pagina con classe square e come contenuto il numero iterazione
+for (let i = 1; i <= 100; i++) {
+
+    console.log(i);
+
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.append(i);
+
     // - verifico se il numero del quadrato è divisibile per 3
         // * aggiungo la classe fizz e modifico il contenuto con la parole fizz
     // - verifico se il numero del quadrato è divisibile per 5        
         // * aggiungo la classe buzz e modifico il contenuto con la parole buzz
     // - se il numero è divisibile per 3 e per 5 aggiungo entrambe le classi ed entrambe le parole
+    if (i % 3 === 0 && i % 5 === 0) {
+        square.classList.add("fizz");
+        square.classList.add("buzz");
+        square.innerHTML = "fizzbuzz";
+    } else if (i % 3 === 0) {
+        square.classList.add("fizz");
+        square.innerHTML = "fizz";
+    } else if (i % 5 === 0) {
+        square.classList.add("buzz");
+        square.innerHTML = "buzz";
+    };
+
+    // aggiungo il div in pagina
+    squareContainer.append(square);
+}
